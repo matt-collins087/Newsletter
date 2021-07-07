3b630f5506064812c9e3e177810dde80fc36b43e
Matt <matt.collins087@gmail.com>
2021-07-07 14:37:23 -0500

    Connected Mailchimp api with successful post requests for new contacts

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+require('dotenv').config();
 
 const app = express();
 
@@ -11,9 +14,41 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-  let firstName = req.body.fname;
-  let lastName = req.body.lname;
-  let email = req.body.email;
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: 'subscribed',
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+  const API_KEY = process.env.API_KEY;
+
+  const url = 'https://us6.api.mailchimp.com/3.0/lists/6b2754996c';
+
+  const options = {
+    method: 'POST',
+    auth: `matt:${API_KEY}`,
+  }
+
+  const request = https.request(url, options, (response) => {
+    response.on('data', (data) => {
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
 });
 
 app.listen(3000, () => {
